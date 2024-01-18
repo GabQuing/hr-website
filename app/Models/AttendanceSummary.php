4f8d8a2787d0c7c8a699da2dd8790d359f4bc437
Camerone29 <gab.quing29@gmail.com>
2024-01-18 10:02:03 +0800

    add function to models

--- app/Models/AttendanceSummary.php
+++ app/Models/AttendanceSummary.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use DB;
+use DateTime;
 class AttendanceSummary extends Model
 {
     use HasFactory;
@@ -14,5 +15,41 @@ class AttendanceSummary extends Model
     public function getByDate($date, $user_id)
     {
         return $this->where('user_id', $user_id)->where('log_date', $date)->first();
+    }
+
+    public function countPresentDays($user_id, $fromDate, $toDate) {
+        return $this->where('user_id',$user_id)
+            ->whereBetween('log_date', [$fromDate,$toDate])
+            ->whereNotNull('clock_in')
+            ->whereNotNull('clock_out')
+            ->count();
+    }
+    public function countAbsentDays($user_id, $fromDate, $toDate) {
+        $present_dates = $this->where('user_id',$user_id)
+            ->whereBetween('log_date', [$fromDate,$toDate])
+            ->whereNotNull('clock_in')
+            ->whereNotNull('clock_out')
+            ->pluck('log_date')
+            ->toArray();
+
+        $dates = [];
+        $absent_count = 0;
+        if ($fromDate <= $toDate) {
+            $dates[] = $fromDate;
+            while ($fromDate < $toDate) {
+                $newDateString = date('Y-m-d', strtotime($fromDate . ' +1 day'));
+
+                $dates[] = $newDateString;
+                $fromDate = $newDateString;
+            }
+        }
+
+        foreach ($dates as $date) {
+            if (!in_array($date, $present_dates)) {
+                $absent_count++;
+            }
+        }
+
+        return $absent_count;
     }
 }
--- app/Models/UserLog.php
+++ app/Models/UserLog.php
@@ -10,6 +10,29 @@ class UserLog extends Model
     use HasFactory;
     protected $guarded = [];
 
+    public function getAllLogs() {
+        return $this->leftJoin('users', 'users.id', 'user_logs.user_id')
+            ->leftJoin('log_types', 'log_types.id', 'user_logs.log_type_id')
+            ->select(
+                'user_logs.*',
+                'users.name as user_name',
+                'log_types.description as log_type_description',
+            )
+            ->orderBy('id', 'desc');
+    }
+
+    public function getByUserId($user_id) {
+        return $this->leftJoin('users', 'users.id', 'user_logs.user_id')
+            ->leftJoin('log_types', 'log_types.id', 'user_logs.log_type_id')
+            ->select(
+                'user_logs.*',
+                'users.name as user_name',
+                'log_types.description as log_type_description',
+            )
+            ->where('users.id', $user_id)
+            ->orderBy('id', 'desc');
+    }
+
     public function getDetails()
     {
         return $this->where('user_logs.id', $this->id)
@@ -22,4 +45,5 @@ class UserLog extends Model
             ->leftJoin('users', 'users.id', 'user_logs.user_id')
             ->first();
     }
+    
 }
