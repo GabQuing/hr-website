f80954a94a1c0dfb72946f8839ff398a2c3f5a42
Camerone29 <gab.quing29@gmail.com>
2024-01-26 11:57:22 +0800

    fixing welcome blade not done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,78 +4,79 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>DTC Attendance System</title>
+    <title>HR Attendance System</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     {{-- Favicon --}}
-    <link rel="icon" type="image/x-icon" href="{{ asset('/img/digits_logo.png') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('/img/Icon.png') }}">
 
 </head>
 <style>
-    
     *{
         padding: 0;
         margin: 0;
         box-sizing: border-box;
     }
-
     .section{
         height: 100vh;
     }
-
-    
+    .img_container{
+    height: auto;
+    width: 100%;
+    padding: 20px;
+    }
+    .img_container img{
+        width: 100%
+    }
+    .welcome_title{
+        margin-bottom: 5px;
+    }
+    .welcome_text,
+    .aboard_text{
+        font-size: 100px;
+        font-weight: 700;
+        color: #47A5C4;
+        text-align: center;
+        letter-spacing: 8px;
+        margin: -25px;
+    }
+    .aboard_text{
+        color: #0F1F27;
+    }
+    .employee_text{
+        font-size: 80px;
+        color: #0F1F27;
+        letter-spacing: 8px;
+        font-weight: 650;
+    }
+    .welcome_description{
+        padding: 50px 150px 0px 150px;
+        text-align: center;
+    }
 </style>
 
 <body>
     <section>
         <div class="welcome_content">
             <div class="welcome_content_header">
-                <img src="{{ asset('img/Sign_up_picture.jpg') }}" alt="">
+                <div class="img_container">
+                    <img src="{{ asset('img/bossman.png') }}" alt="">
+                </div>
             </div>
             <div class="welcome_content_body">
-                <div class="welcome_content_body_header">
-                    <p>Welcome To DTC Attendance Monitoring System</p>
-                </div>
-                <div class="welcome_content_body_description">
-                    <p> The Attendance Monitoring System uses cutting-edge face recognition attendance system, 
-                        designed to streamline your attendance tracking and management process. 
-                        Our system utilizes advanced facial recognition technology to accurately 
-                        capture employee attendance in real-time, eliminating the need for manual 
-                        timekeeping and reducing errors.
-                    </p>
-                </div>
-                <div class = "welcome_content_instruction">
-                    <div class = "welcome_column1">
-                        <img src="{{ asset('img/changePassword.jpg') }}" alt="">
-                        <div class="content_description">
-                            <div class="content_description_header">
-                                <strong>Step One: Password Settings</strong>
-                            </div>
-                            <div class="contenct_description_body">
-                                You will need to change the temporary password of your account, the new password must be at least more than 8 characters.
-                            </div>
-                        </div>
+                <div class="welcome_title">
+                    <div class="welcome_text">
+                        <span>WELCOME</span>
                     </div>
-                    <div class = "welcome_column2">
-                        <img src="{{ asset('img/registerFace.jpg') }}" alt="">
-                        <div class="content_description">
-                            <div class="content_description_header">
-                                <strong>Step Two: Face Biometric</strong>
-                            </div>
-                            <div class="contenct_description_body">
-                                You will need to register your facial structure so the system can identify your face biometric and register your time logs.
-                            </div>
-                        </div>
+                    <div class="aboard_text">
+                        <span>ABOARD</span>
                     </div>
-                    <div class = "welcome_column3">
-                        <img src="{{ asset('img/waitApproval.jpg') }}" alt="">
-                        <div class="content_description">
-                            <div class="content_description_header">
-                            <strong>Step Three: Admin's Approval</strong>
-                            </div>
-                            <div class="contenct_description_body">
-                                All requests from the DTC Attendance System are being monitored by the Admins. All information will be checked before using the Face Recognition Time log System.
-                            </div>
-                        </div>
+                    <div class="employee_text">
+                        <span>OUR EMPLOYEE!</span>
+                    </div>
+                </div>
+                <div class="welcome_description">
+                    <div>
+                        <p>Thank you for log in for the very first time to our HR attendance System. We will be using this website to keep in check our daily attendances, monitor our schedule profiles and employee information. The system will help the administration stay connected to their employee. Please press "Get Started" to proceed in changing the password.  </p>
                     </div>
                 </div>
                 <div class="home_btns">
@@ -83,7 +84,7 @@
                         <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
                             @auth
                                 <br>
-                                <a href="{{ route('newProfile', auth()->user()->id) }}" class="h_btn">Continue</a>
+                                <a href="{{ route('newProfile', auth()->user()->id) }}" class="h_btn">Get Started</a>
                             @else
                                 <br>
                                 <a href="{{ route('login') }}" class="h_btn">Login Here</a>
