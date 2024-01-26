d4419fa25af5af3e8d284d1df6561623d2484275
patrickDigits <patrickpunzalan@digits.ph>
2024-01-26 18:19:22 +0800

    setup password for user

--- resources/views/change_password.blade.php
+++ resources/views/change_password.blade.php
@@ -5,8 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Account Setting</title>
-    <!-- Css Link -->
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    {{-- Favicon --}}
+    <link rel="icon" type="image/x-icon" href="{{ asset('/img/icon.png') }}">
+    {{-- CSS --}}
+    <link rel="stylesheet" href="{{ asset('css/change-password.css') }}">
     <!-- Jquery Link -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
     <!-- Jquery Modal -->
@@ -14,11 +16,111 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css" />
     {{-- Favicon --}}
     <link rel="icon" type="image/x-icon" href="{{ asset('/img/digits_logo.png') }}">
-
 </head>
 <body>
     <!-- Register Content -->
-    <section class="register">
+    <section class="register-section">
+        <div class="register-box">
+            <form method="POST" action="{{ route('new_password', auth()->user()->id)}}">
+                @csrf
+                <div class="register-header">
+                    <div class="register-header-process rh-active u-fw-b">
+                        <div class="register-number">
+                            <span>1</span>
+                            <h5>Update Your Password</h5>
+                        </div>
+                    </div>
+                    <div class="register-header-process u-fw-b">
+                        <div class="register-number">
+                            <span>2</span>
+                            <h5>Password Updated</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="register-content">
+                    {{-- <div class="register-content-header">
+                        <h3 class="u-fw-b u-c-light-gray">Step 1</h3>
+                        <h4 class="u-fw-b u-c-dark-gray">Change Your Password</h4>
+                        <h5 class="u-fw-b u-c-gray">After your successful registration, you may now change your password.</h5>
+                    </div>
+                    <br>
+                    <div class="register-form">
+                        <div class="d-flex">
+                            <div class="form-input">
+                                <h5 class="register-label u-fw-b" for="">First Name <span class="c-danger">*</span></h5>
+                                <input class="register-input" type="text" name="first_name" value="{{ $user_info->first_name }}" placeholder="Enter your first name" readonly>
+                            </div>
+                            <div class="form-input">
+                                <h5 class="register-label u-fw-b" for="">Last Name <span class="c-danger">*</span></h5>
+                                <input class="register-input" type="text" name="last_name"  value="{{ $user_info->last_name }}" placeholder="Enter your last name" readonly>
+                            </div>
+                        </div>
+                        <div class="d-flex">
+                            <div class="form-input">
+                                <h5 class="register-label u-fw-b" for="">Email <span class="c-danger">*</span></h5>
+                                <input class="register-input" type="email" name="email" value="{{ $user_info->email }}" readonly>
+                            </div>
+                            <div class="form-input">
+                                <h5 class="register-label u-fw-b" for="">Mobile Number <span class="c-danger">*</span></h5>
+                                <input class="register-input" type="text" name="mobile_number"  placeholder="09*********" pattern="[0-9]{4}[0-9]{3}[0-9]{4}" required>
+                            </div>
+                        </div>
+                        <div class="d-flex">
+                            <div class="form-input">
+                                <h5 class="register-label u-fw-b" for="">New Password <span class="c-danger">*</span></h5>
+                                <input class="register-input" type="password" name="password" placeholder="Enter password" required>
+                                @if ($errors->has('password'))
+                                    <h6 class="c-danger">{{ $errors->first('password') }}</h6>
+                                @endif                        
+                            </div>
+                            <div class="form-input">
+                                <h5 class="register-label u-fw-b" for="">Confirm Password <span class="c-danger">*</span></h5>
+                                <input class="register-input" type="password" name="password_confirmation" placeholder="Type your password again" required>
+                                @if ($errors->has('password_confirmation'))
+                                    <h6 class="c-danger">{{ $errors->first('password_confirmation') }}</h6>
+                                @endif                        
+                            </div>
+                        </div>
+                        <div class="d-flex" style="margin-top: 5px">
+                            <span>
+                                <input class="r_radio_btn" style="position: relative; top: 1px;" type="checkbox" required>
+                                You agree with our <a class="register_links" href="#ex1" rel="modal:open">Terms of Service, and Privacy Policy.</a>
+                            </span>                        
+                        </div>
+                        <!-- Terms of service modal -->
+                        <div id="ex1" class="modal">
+                            <p>Terms of Service and Privacy Policy.</p>
+                            <br>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem consectetur qui adipisci ipsam sint officia rem aspernatur vero aperiam corporis. Assumenda ratione quia laudantium cumque? Corporis obcaecati magnam, inventore vitae animi modi quia, omnis voluptas, dicta a architecto magni amet fugiat laboriosam id porro corrupti nihil doloribus est incidunt. Dolorum!</p>
+                            <br>
+                            <a href="#" rel="modal:close">Close</a>
+                        </div>
+                        <button class="" id="btn-continue">Submit</button>
+                    </div> --}}
+                    <div>
+                        <div class="register-content-header">
+                            <h4 class="u-fw-b u-c-dark-gray">Password Updated</h4>
+                            <h5 class="u-fw-b u-c-gray">Congratulations! You have successfully created your account. Welcome to our team!</h5>
+                            <br>
+                            <img src="{{ asset('img/success.png') }}" alt="" style="margin: auto; object-fit: contain; width: 100%; max-width: 100px;">
+                            <br>
+                            <h5 class="u-c-gray" style="width: 100%; max-width: 800px;">
+                                We are thrilled to have you on board. Your journey with us is just beginning, and we look forward to working together to achieve great things. Feel free to explore your account dashboard and discover the exciting features and opportunities that await you.
+                            </h5>
+                            <br>
+                            <h5 class="u-c-gray" style="width: 100%; max-width: 800px;">
+                                Should you have any questions or need assistance, our support team is here to help. Don't hesitate to reach out, and we'll ensure a smooth and enjoyable experience for you.
+                            </h5>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </section>
+    {{-- <img src="{{ asset('img/change-password-bg.jpg') }}" alt="" style="height: 100%; width: 100%; position: fixed; z-index: -1; object-fit: cover; background-repeat: no-repeat; "> --}}
+
+
+    {{-- <section class="register">
         <form method="POST" action="{{ route('new_password', auth()->user()->id)}}">
             {{ csrf_field() }}
             <div class="register_content">
@@ -57,7 +159,7 @@
                                 <div class="d-flex">
                                     <div class="d-iblock">
                                         @if ($errors->has('mobile_number'))
-                                            <span class="text-danger">{{ $errors->first('mobile_number') }}</span>
+                                            <span class="c-danger">{{ $errors->first('mobile_number') }}</span>
                                         @endif 
                                     </div>
                                 </div>
@@ -66,14 +168,14 @@
                                 <label for="password" >New Password</label>
                                 <input class="r_input" type="password" name="password" placeholder="Enter password" required>
                                 @if ($errors->has('password'))
-                                    <span class="text-danger">{{ $errors->first('password') }}</span>
+                                    <span class="c-danger">{{ $errors->first('password') }}</span>
                                 @endif
                             </div>
                             <div class="register_input_content">
                                 <label for="password-confirm">Confirm New Password</label>
                                 <input class="r_input" type="password" name="password_confirmation" placeholder="Type your password again" required>
                                 @if ($errors->has('password_confirmation'))
-                                    <span class="text-danger">{{ $errors->first('password_confirmation') }}</span>
+                                    <span class="c-danger">{{ $errors->first('password_confirmation') }}</span>
                                 @endif
                             </div>
                             <div class="register_input_content">
@@ -110,7 +212,7 @@
                 </div>
             </div>
         </form>
-    </section>
+    </section> --}}
 
     <script>
 
