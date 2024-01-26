ee1255c026e132be6b0fe89edccc14d35ad168cc
patrickDigits <patrickpunzalan@digits.ph>
2024-01-26 18:55:57 +0800

    update css

--- resources/views/change_password.blade.php
+++ resources/views/change_password.blade.php
@@ -23,14 +23,14 @@
         <div class="register-box">
             <form method="POST" action="{{ route('new_password', auth()->user()->id)}}">
                 @csrf
-                <div class="register-header">
-                    <div class="register-header-process rh-active u-fw-b">
+                <div class="register-header {{ session('success') ? 'rh-active1' : '' }}">
+                    <div class="register-header-process u-fw-b {{ session('success') ? 'rh-not-active' : 'rh-active' }}">
                         <div class="register-number">
                             <span>1</span>
                             <h5>Update Your Password</h5>
                         </div>
                     </div>
-                    <div class="register-header-process u-fw-b">
+                    <div class="register-header-process u-fw-b {{ session('success') ? 'rh-active1' : '' }}">
                         <div class="register-number">
                             <span>2</span>
                             <h5>Password Updated</h5>
@@ -38,77 +38,78 @@
                     </div>
                 </div>
                 <div class="register-content">
-                    {{-- <div class="register-content-header">
+                    <div class="register-content-header">
                         <h3 class="u-fw-b u-c-light-gray">Step 1</h3>
                         <h4 class="u-fw-b u-c-dark-gray">Change Your Password</h4>
                         <h5 class="u-fw-b u-c-gray">After your successful registration, you may now change your password.</h5>
                     </div>
                     <br>
-                    <div class="register-form">
-                        <div class="d-flex">
-                            <div class="form-input">
-                                <h5 class="register-label u-fw-b" for="">First Name <span class="c-danger">*</span></h5>
-                                <input class="register-input" type="text" name="first_name" value="{{ $user_info->first_name }}" placeholder="Enter your first name" readonly>
+                    @if (!session('success'))
+                        <div class="register-form">
+                            <div class="d-flex">
+                                <div class="form-input">
+                                    <h5 class="register-label u-fw-b" for="">First Name <span class="c-danger">*</span></h5>
+                                    <input class="register-input" type="text" name="first_name" value="{{ $user_info->first_name }}" placeholder="Enter your first name" readonly>
+                                </div>
+                                <div class="form-input">
+                                    <h5 class="register-label u-fw-b" for="">Last Name <span class="c-danger">*</span></h5>
+                                    <input class="register-input" type="text" name="last_name"  value="{{ $user_info->last_name }}" placeholder="Enter your last name" readonly>
+                                </div>
                             </div>
-                            <div class="form-input">
-                                <h5 class="register-label u-fw-b" for="">Last Name <span class="c-danger">*</span></h5>
-                                <input class="register-input" type="text" name="last_name"  value="{{ $user_info->last_name }}" placeholder="Enter your last name" readonly>
+                            <div class="d-flex">
+                                <div class="form-input">
+                                    <h5 class="register-label u-fw-b" for="">Email <span class="c-danger">*</span></h5>
+                                    <input class="register-input" type="email" name="email" value="{{ $user_info->email }}" readonly>
+                                </div>
+                                <div class="form-input">
+                                    <h5 class="register-label u-fw-b" for="">Mobile Number <span class="c-danger">*</span></h5>
+                                    <input class="register-input" type="text" name="mobile_number"  placeholder="09*********" pattern="[0-9]{4}[0-9]{3}[0-9]{4}" required>
+                                </div>
                             </div>
-                        </div>
-                        <div class="d-flex">
-                            <div class="form-input">
-                                <h5 class="register-label u-fw-b" for="">Email <span class="c-danger">*</span></h5>
-                                <input class="register-input" type="email" name="email" value="{{ $user_info->email }}" readonly>
+                            <div class="d-flex">
+                                <div class="form-input">
+                                    <h5 class="register-label u-fw-b" for="">New Password <span class="c-danger">*</span></h5>
+                                    <input class="register-input" type="password" name="password" placeholder="Enter password" required>
+                                    @if ($errors->has('password'))
+                                        <h6 class="c-danger">{{ $errors->first('password') }}</h6>
+                                    @endif                        
+                                </div>
+                                <div class="form-input">
+                                    <h5 class="register-label u-fw-b" for="">Confirm Password <span class="c-danger">*</span></h5>
+                                    <input class="register-input" type="password" name="password_confirmation" placeholder="Type your password again" required>
+                                    @if ($errors->has('password_confirmation'))
+                                        <h6 class="c-danger">{{ $errors->first('password_confirmation') }}</h6>
+                                    @endif                        
+                                </div>
                             </div>
-                            <div class="form-input">
-                                <h5 class="register-label u-fw-b" for="">Mobile Number <span class="c-danger">*</span></h5>
-                                <input class="register-input" type="text" name="mobile_number"  placeholder="09*********" pattern="[0-9]{4}[0-9]{3}[0-9]{4}" required>
+                            <div class="d-flex" style="margin-top: 5px">
+                                <span>
+                                    <input class="r_radio_btn" style="position: relative; top: 1px;" type="checkbox" required>
+                                    You agree with our <a class="register_links" href="#ex1" rel="modal:open">Terms of Service, and Privacy Policy.</a>
+                                </span>                        
                             </div>
-                        </div>
-                        <div class="d-flex">
-                            <div class="form-input">
-                                <h5 class="register-label u-fw-b" for="">New Password <span class="c-danger">*</span></h5>
-                                <input class="register-input" type="password" name="password" placeholder="Enter password" required>
-                                @if ($errors->has('password'))
-                                    <h6 class="c-danger">{{ $errors->first('password') }}</h6>
-                                @endif                        
+                            <!-- Terms of service modal -->
+                            <div id="ex1" class="modal">
+                                <p>Terms of Service and Privacy Policy.</p>
+                                <br>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem consectetur qui adipisci ipsam sint officia rem aspernatur vero aperiam corporis. Assumenda ratione quia laudantium cumque? Corporis obcaecati magnam, inventore vitae animi modi quia, omnis voluptas, dicta a architecto magni amet fugiat laboriosam id porro corrupti nihil doloribus est incidunt. Dolorum!</p>
+                                <br>
+                                <a href="#" rel="modal:close">Close</a>
                             </div>
-                            <div class="form-input">
-                                <h5 class="register-label u-fw-b" for="">Confirm Password <span class="c-danger">*</span></h5>
-                                <input class="register-input" type="password" name="password_confirmation" placeholder="Type your password again" required>
-                                @if ($errors->has('password_confirmation'))
-                                    <h6 class="c-danger">{{ $errors->first('password_confirmation') }}</h6>
-                                @endif                        
+                            <button class="" id="btn-continue">Submit</button>
+                        </div>
+                        @else
+                            <div class="register-content-header">
+                                <h4 class="u-fw-b u-c-dark-gray">Password Updated</h4>
+                                <br>
+                                <img src="{{ asset('img/success.png') }}" alt="" style="margin: auto; object-fit: contain; width: 100%; max-width: 100px;">
+                                <br>
+                                <h5 class="u-c-gray" style="width: 100%; max-width: 580px; text-align: center;">
+                                    Your account is now under review by our admin team. Please await approval. If you have any questions, feel free to reach out to our support team.
+                                </h5>
+                                <br>
                             </div>
-                        </div>
-                        <div class="d-flex" style="margin-top: 5px">
-                            <span>
-                                <input class="r_radio_btn" style="position: relative; top: 1px;" type="checkbox" required>
-                                You agree with our <a class="register_links" href="#ex1" rel="modal:open">Terms of Service, and Privacy Policy.</a>
-                            </span>                        
-                        </div>
-                        <!-- Terms of service modal -->
-                        <div id="ex1" class="modal">
-                            <p>Terms of Service and Privacy Policy.</p>
-                            <br>
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem consectetur qui adipisci ipsam sint officia rem aspernatur vero aperiam corporis. Assumenda ratione quia laudantium cumque? Corporis obcaecati magnam, inventore vitae animi modi quia, omnis voluptas, dicta a architecto magni amet fugiat laboriosam id porro corrupti nihil doloribus est incidunt. Dolorum!</p>
-                            <br>
-                            <a href="#" rel="modal:close">Close</a>
-                        </div>
-                        <button class="" id="btn-continue">Submit</button>
-                    </div> --}}
-                    <div>
-                        <div class="register-content-header">
-                            <h4 class="u-fw-b u-c-dark-gray">Password Updated</h4>
-                            <br>
-                            <img src="{{ asset('img/success.png') }}" alt="" style="margin: auto; object-fit: contain; width: 100%; max-width: 100px;">
-                            <br>
-                            <h5 class="u-c-gray" style="width: 100%; max-width: 580px; text-align: center;">
-                                Your account is now under review by our admin team. Please await approval. If you have any questions, feel free to reach out to our support team.
-                            </h5>
-                            <br>
-                        </div>
-                    </div>
+                    @endif
                 </div>
             </form>
         </div>
@@ -225,11 +226,11 @@
         // $(".string_only").on("input", function() {
         //     $(this).val($(this).val().replace(/[0-9]/g, ''));
         // });
-        if ("{{ session('success') }}"){
-            setTimeout(function(){
-                window.location.href = "{{ route('logout') }}"
-            }, 2000);
-        }
+        // if ("{{ session('success') }}"){
+        //     setTimeout(function(){
+        //         window.location.href = "{{ route('logout') }}"
+        //     }, 2000);
+        // }
         
 
     </script>
