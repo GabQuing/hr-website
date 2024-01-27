bf4af07112efd412dbad6e98b8f6062327491002
Camerone29 <gab.quing29@gmail.com>
2024-01-27 14:38:52 +0800

    fix welcome blade and css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,38 +19,106 @@
     .section{
         height: 100vh;
     }
-    .img_container{
+    .img_container {
     height: auto;
     width: 100%;
-    padding: 20px;
-    }
+    padding: 20px 20px, 0px 20px;
+    margin-top: auto; /* Move to the bottom */
+}
     .img_container img{
         width: 100%
     }
-    .welcome_title{
-        margin-bottom: 5px;
+    .img_container_logo{
+        max-width: 100%;
+        width: 400px;
+        padding: 20px 20px 0px 20px;
+        margin-top: 20px;
     }
+    .img_container_logo img {
+    width: 100%; /* set the desired width */
+}
     .welcome_text,
     .aboard_text{
-        font-size: 100px;
+        font-size: 70px;
         font-weight: 700;
         color: #47A5C4;
         text-align: center;
-        letter-spacing: 8px;
+        letter-spacing: 5px;
         margin: -25px;
     }
     .aboard_text{
         color: #0F1F27;
     }
     .employee_text{
-        font-size: 80px;
+        font-size: 60px;
         color: #0F1F27;
-        letter-spacing: 8px;
-        font-weight: 650;
+        font-weight: 600;
+        text-align: center;
     }
     .welcome_description{
-        padding: 50px 150px 0px 150px;
-        text-align: center;
+        padding: 20px 50px 0px 50px;
+    }
+    @media only screen and (max-width: 1273px) {
+        .welcome_text,
+        .aboard_text{
+            font-size: 60px;
+            margin: -15px;
+        }
+        .employee_text{
+            font-size: 50px;
+        }
+    }
+    @media only screen and (max-width: 1160px) {
+        .welcome_content{
+            flex-direction: column;
+        }
+        .welcome_content_header{
+            flex-direction: row;
+        }
+        .img_container{
+            width: 50%
+        }
+        .welcome_content_body{
+            width: 100%;
+            padding: 100px;
+
+        }
+    }
+    @media only screen and (max-width: 653px) {
+        .welcome_content_body{
+            padding: 20px 0px 0px 0px;
+        }
+        .welcome_text,
+        .aboard_text{
+            font-size: 50px;
+            margin: -15px;
+        }
+        .employee_text{
+            font-size: 40px;
+        }
+        .img_container_logo{
+            margin-top: 0px;
+        }
+    }
+
+    @media only screen and (max-width: 394px) {
+        .welcome_content_body{
+            padding: 20px 0px 0px 0px;
+        }
+        .welcome_description{
+        padding: 20px 20px 0px 20px;
+    }
+        .welcome_text,
+        .aboard_text{
+            font-size: 40px;
+            margin: -5px;
+        }
+        .employee_text{
+            font-size: 30px;
+        }
+        .img_container_logo{
+            margin-top: 0px;
+        }
     }
 </style>
 
@@ -58,6 +126,9 @@
     <section>
         <div class="welcome_content">
             <div class="welcome_content_header">
+                <div class="img_container_logo">
+                    <img src="{{ asset('img/logo_white.png') }}" alt="">
+                </div>
                 <div class="img_container">
                     <img src="{{ asset('img/bossman.png') }}" alt="">
                 </div>
@@ -71,12 +142,12 @@
                         <span>ABOARD</span>
                     </div>
                     <div class="employee_text">
-                        <span>OUR EMPLOYEE!</span>
+                        <span>TO OUR EMPLOYEE!</span>
                     </div>
                 </div>
                 <div class="welcome_description">
                     <div>
-                        <p>Thank you for log in for the very first time to our HR attendance System. We will be using this website to keep in check our daily attendances, monitor our schedule profiles and employee information. The system will help the administration stay connected to their employee. Please press "Get Started" to proceed in changing the password.  </p>
+                        <p>"Thank you for logging in for the very first time to our HR Attendance System. We will be using this website to keep track of our daily attendance, monitor schedule profiles, and manage employee information. The system will help the administration stay connected with their employees. Please press 'Get Started' to proceed with changing your password."</p>
                     </div>
                 </div>
                 <div class="home_btns">
