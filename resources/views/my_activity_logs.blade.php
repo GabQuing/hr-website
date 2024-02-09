cf00e1c1a56993377adb359e8a9cf7186e6e19d5
Camerone29 <gab.quing29@gmail.com>
2024-02-09 16:05:04 +0800

    adjustment for the front end

--- resources/views/my_activity_logs.blade.php
+++ resources/views/my_activity_logs.blade.php
@@ -4,31 +4,29 @@
 
 @section('content')
 
-<div>
-    <div class="modal" id="pop_image">
-        <form method="POST" action="{{ route('generate_file') }}">
-        @csrf
-            <div class="modal_body" >
-                <input type="text" name="employeeName" value="{{ auth()->user()->employee_name }}" style="display: none;"> 
-                <div class="container_title">
-                    <p class="header_title_h2">My Activity Logs Summary</p>
+<div class="modal" id="pop_image">
+    <form method="POST" action="{{ route('generate_file') }}">
+    @csrf
+        <div class="modal_body" >
+            <input type="text" name="employeeName" value="{{ auth()->user()->employee_name }}" style="display: none;"> 
+            <div class="container_title">
+                <p class="header_title_h2">My Activity Logs Summary</p>
+            </div>
+            <div class="summary_report">
+                <div class="sr_from_date">
+                    <label for="">From:</label>
+                    <input type="date" name="from_date" id="from_date" value="{{ $from ?? '' }}" required>
                 </div>
-                <div class="summary_report">
-                    <div class="sr_from_date">
-                        <label for="">From:</label>
-                        <input type="date" name="from_date" id="from_date" value="{{ $from ?? '' }}" required>
-                    </div>
-                    <div class="sr_to_date">
-                        <label for="">To:</label>
-                        <input type="date" name="to_date" id="to_date" value="{{ $to ?? '' }}" required>
-                    </div>
-                </div>
-                <div class="datebtn">
-                    <button type="submit" class="date_generate">Generate</button>
+                <div class="sr_to_date">
+                    <label for="">To:</label>
+                    <input type="date" name="to_date" id="to_date" value="{{ $to ?? '' }}" required>
                 </div>
             </div>
-        </form>
-    </div>
+            <div class="datebtn">
+                <button type="submit" class="date_generate">Generate</button>
+            </div>
+        </div>
+    </form>
 </div>
 
 <a class="user_info_link" href="#pop_image" rel="modal:open">Generate File</a>
