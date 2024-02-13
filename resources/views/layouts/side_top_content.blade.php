0049ffe20073e5805d6a1a666a9ec1b358be6a12
Camerone29 <gab.quing29@gmail.com>
2024-02-13 10:34:10 +0800

    update favicon logo

--- resources/views/layouts/side_top_content.blade.php
+++ resources/views/layouts/side_top_content.blade.php
@@ -14,7 +14,7 @@
     <!-- Jquery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
     {{-- Favicon --}}
-    <link rel="icon" type="image/x-icon" href="{{ asset('/img/icon.png') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('/img/Icon.png') }}">
     {{-- Select2 --}}
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
