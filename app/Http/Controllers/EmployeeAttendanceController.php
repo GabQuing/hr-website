ef678b5d9e17e972444c8b8ffa6de00c8d94656f
GabQuing <gab.quing29@gmail.com>
2025-02-19 04:27:55 +0800

    unfinished employee attendance tracker

--- app/Http/Controllers/EmployeeAttendanceController.php
+++ app/Http/Controllers/EmployeeAttendanceController.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\DB;
 use App\Models\User;
 use App\Models\UserLog;
 use Throwable;
+use Carbon\Carbon;
+
 
 class EmployeeAttendanceController extends Controller
 {
@@ -52,9 +54,62 @@ class EmployeeAttendanceController extends Controller
             }
             $data['params'] = $request->all();
             $data['summary_data'] = $summary_data;
+            // dd($data['summary_data']);
             return view('employee_attendance', $data);
         } catch (Throwable $e) {
             return 'Something went wrong. Please check if the users have schedule.';
         }
     }
+
+    //     public function employeeDaysPresent(Request $request)
+    // {
+    //     try {
+    //         $data['year'] = date('Y');
+    //         $data['daysOfWeek'] = ['Sa', 'Su', 'Mo', 'Tu', 'We', 'Th', 'Fr'];
+    //         $data['usernames'] = (new User())
+    //             ->getAllActiveUsers()
+    //             ->select('users.id', 'users.name')
+    //             ->orderBy('users.name', 'asc')
+    //             ->get();
+    //         $fromDate = date_create($request->input('from_date'));
+    //         $toDate = date_create($request->input('to_date'));
+    //         $userIds = $request->input('users_id');
+    //         $controller = new AttendanceController();
+    //         $summary_data = [];
+    //         foreach ($userIds as $userId) {
+    //             $summary_data[] = $controller->getSummaryData($userId, date_format($fromDate, 'Y-m-d'), date_format($toDate, 'Y-m-d'));
+    //         }
+    
+    //         // Generate months dynamically
+    //         $filteredMonths = [];
+    //         $start = clone $fromDate;
+    
+    //         while ($start <= $toDate) {
+    //             $month = date_format($start, 'm'); // Get month number
+    //             $year = date_format($start, 'Y'); // Get year
+    //             $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+    //             $firstDayOfMonth = date('w', strtotime("$year-$month-01")); // Get first day (0 = Sunday, 6 = Saturday)
+    
+    //             $filteredMonths[] = [
+    //                 'month' => date_format($start, 'M'),
+    //                 'year' => $year,
+    //                 'daysInMonth' => $daysInMonth,
+    //                 'firstDayOfMonth' => $firstDayOfMonth,
+    //             ];
+    
+    //             // Move to the next month
+    //             date_add($start, date_interval_create_from_date_string("1 month"));
+    //         }
+    
+    //         $data['params'] = $request->all();
+    //         $data['summary_data'] = $summary_data;
+    //         $data['filtered_months'] = $filteredMonths; // Pass dynamically generated months
+    
+    //         dd($data['summary_data'], $data['filtered_months']);
+    //         return view('employee_attendance', $data);
+    //     } catch (Throwable $e) {
+    //         return 'Something went wrong. Please check if the users have a schedule.';
+    //     }
+    // }
+    
 }
