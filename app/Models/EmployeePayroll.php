76f13ffff1fb64b1a9ba385e2cc917c3b14f665e
Fil <filongunio@gmail.com>
2025-03-29 00:52:10 +0800

    add migration for paydate in payroll

--- app/Models/EmployeePayroll.php
+++ app/Models/EmployeePayroll.php
@@ -4,12 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class EmployeePayroll extends Model
 {
     use HasFactory;
-    
+
     protected $guarded = [];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
     public function updatedBy()
     {
